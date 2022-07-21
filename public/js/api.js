b7b394209f1316ac344c2f76130ff2e10360f895
Prince Nkem Konwea <84895081+kokodecontee7@users.noreply.github.com>
2022-07-22 11:56:34 +1200

    Add files via upload
    initial deployment of complete netflix-clone web app

---
+++ public/js/api.js
@@ -0,0 +1,7 @@
+let api_key = "13025b52e1f4cf2cbc8fff9608f15b6d";
+
+let img_url = "https://image.tmdb.org/t/p/w500";
+let original_img_url = "https://image.tmdb.org/t/p/original";
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?";
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?";
+let movie_detail_http = "https://api.themoviedb.org/3/movie";
